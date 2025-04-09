e4823af7486b13123acde6defa08a195ac224e09
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-04-09 23:25:14 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <div className='bg-night-bg w-full h-[100vh]'>
+    <h1 className='text-2xl font-bold text-center text-night-text'>Hello World!</h1>
+  </div>
+  )
+}
+
+export default App
