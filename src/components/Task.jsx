86dad19b912ce49c375b30bdce91dde9a0fe02d6
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2025-04-27 13:36:03 +0300

    update: task.jsx, theme.css
    - Delete and Done buttons are added to Task
    - colors for Delete and Done buttons are added in theme (safe, caution, safe-hover, caution-hover)

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,29 @@
+import { useRef } from "react";
+
 export default function Task(props) {
+
     console.log("props -->", props)
     const task = props.task;
     return(
         <div className={`col-span-1 row-span-1 aspect-square text-center 
         flex flex-col justify-center items-center gap-4 bg-day-surface
         dark:bg-night-surface rounded-2xl overflow-hidden
-        text-l font-bold text-day-text dark:text-night-text `}>
+        text-l font-bold text-day-text dark:text-night-text
+        hover:cursor-pointer hover:scale-[103%] relative `}>
+            <button className={`absolute right-[8px] top-[8px] lg:w-[16px]
+            lg:h-[16px] h-[24px] w-[24px] rounded-full bg-day-caution hover:bg-day-caution-hover
+            dark:bg-night-caution dark:hover:bg-night-caution-hover
+            hover:cursor-pointer text-[2xl]
+            flex justify-center items-center `}>
+                <span className="bg-white lg:w-[4px] lg:h-[4px] w-[8px] h-[8px] rounded-full "></span>
+            </button>
+            <button className={`absolute right-[8px] bottom-[8px] lg:w-[16px]
+            lg:h-[16px] h-[24px] w-[24px] rounded-full bg-day-safe hover:bg-day-safe-hover
+            dark:bg-night-safe dark:hover:bg-night-safe-hover
+            hover:cursor-pointer text-[2xl]
+            flex justify-center items-center `}>
+                <span className="bg-white lg:w-[4px] lg:h-[4px] w-[8px] h-[8px] rounded-full "></span>
+            </button>
             <h2 className="">{task.taskname.toUpperCase()}</h2>
             <p className="">{task.focusLevel}</p>
             {/* FOCUS LEVELE GORE IMAJ DOSYASI GELECEK */}
